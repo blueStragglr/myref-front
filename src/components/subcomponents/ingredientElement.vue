baa3f5478b4907421fd0360c7443e0304f0d5bad
jaejae <buskingestagro@gmail.com>
2019-01-09 19:34:53 +0900

    using v-bind

--- src/components/subcomponents/ingredientElement.vue
+++ src/components/subcomponents/ingredientElement.vue
@@ -1,19 +1,32 @@
-<template>
+<template>    
     <div class="ingredient-item">
-      123
+      
+      <img v-bind:src=imagePath />
+      <h2>{{message}}</h2>
+      <br>1개
+      <br>D-7
+
     </div>
 </template>
 
 <script>
+    
   export default {
-    name: "ingredientElement"
+    name: "ingredientElement",
+    data() {
+      return {
+        message : 'Hello',
+        imagePath: "../assets/carrot.png"
+      }      
+    }
+    
   }
 </script>
 
 <style scoped>
   .ingredient-item {
     flex: 0 1 calc(100% / 4 - 20px);
-    border: 1px solid #e9e9e9;
+    border: 0px solid #e9e9e9;
     margin: 10px;
     height: 120px;
     box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.05);
